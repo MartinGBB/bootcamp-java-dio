638fcad03053e2f4e3ef545b65adc0d54293e615
Martin <escorpmartin97@gmail.com>
2023-03-03 20:58:12 -0300

    feat: liga tv com metodos

--- sintaxe-java/metodos/src/SmartTv.java
+++ sintaxe-java/metodos/src/SmartTv.java
@@ -2,4 +2,12 @@ public class SmartTv {
     boolean on = false;
     int volume = 20;
     int channel = 10;
+
+    public void on(String[] args) {
+        on = true;
+    }
+
+    public void off(String[] args) {
+        on = true;
+    }
 }
--- sintaxe-java/metodos/src/User.java
+++ sintaxe-java/metodos/src/User.java
@@ -2,8 +2,14 @@ public class User {
   public static void main(String[] args) throws Exception {
     SmartTv smartTv = new SmartTv();
 
-    System.out.println("ligada: " + smartTv.on);
+    System.out.println("TV ligada: " + smartTv.on);
     System.out.println("volumem: " + smartTv.volume);
     System.out.println("canal: " + smartTv.channel);
+
+    System.out.println("Metodos:");
+
+    smartTv.on(args);
+    System.out.println("TV ligada: " + smartTv.on);
+
   }
 }
