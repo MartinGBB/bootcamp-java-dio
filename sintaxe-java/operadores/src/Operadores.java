05dabc0436fe07d6098adce500c0e19f69d2df8f
Martin <escorpmartin97@gmail.com>
2023-03-02 22:48:39 -0300

    feat: condicional

--- sintaxe-java/operadores/src/Operadores.java
+++ sintaxe-java/operadores/src/Operadores.java
@@ -19,4 +19,25 @@ public class Operadores {
         concat = "1"+(1+1+1);       
         System.out.println(concat);
     }
+
+    public static void condiçao(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+        
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+        
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+        
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+        
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
+    }
+
 }
