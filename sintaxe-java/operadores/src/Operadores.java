3f4f1c540b07ccf7fc415bc00a1e488294c7ddbe
Martin <escorpmartin97@gmail.com>
2023-03-02 23:10:28 -0300

    feat: comparacao

--- sintaxe-java/operadores/src/Operadores.java
+++ sintaxe-java/operadores/src/Operadores.java
@@ -21,6 +21,7 @@ public class Operadores {
 
 
         condiçao(args);
+        compare(args);
     }
 
     public static void condiçao(String[] args) {
@@ -42,4 +43,25 @@ public class Operadores {
       System.out.println("Ternary operator: " + resultTernary);
       
       };
+
+      public static void compare(String[] args) {
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
+        System.out.println("compare:");
+        
+        System.out.println(nome1 == nome2); //true
+
+        String nome3 = new String("JAVA");
+        
+        System.out.println(nome1 == nome3); //false
+
+        String nome4 = nome3;
+
+        System.out.println(nome3 == nome4); //true
+        
+        System.out.println(nome1.equals(nome2)); //true
+        System.out.println(nome2.equals(nome3)); //true
+        System.out.println(nome3.equals(nome4)); //true 
+
+      }
 }
