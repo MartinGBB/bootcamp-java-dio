58156ef2a9a66a6cb35d27f975c57a58e1f65b6b
Martin <escorpmartin97@gmail.com>
2023-03-02 22:58:03 -0300

    feat: condicional com if/else

--- sintaxe-java/operadores/src/Operadores.java
+++ sintaxe-java/operadores/src/Operadores.java
@@ -18,26 +18,25 @@ public class Operadores {
 
         concat = "1"+(1+1+1);       
         System.out.println(concat);
+
+
+        condiçao(args);
     }
 
     public static void condiçao(String[] args) {
-        int numero1 = 1;
-        int numero2 = 2;
-        
-        if(numero1 > numero2)
-            System.out.print("Numero 1 maior que numero 2");
-        
-        if(numero1 < numero2)
-            System.out.print("Numero 1 menor que numero 2");
-        
-        if(numero1 >= numero2)
-            System.out.print("Numero 1 maior ou igual que numero 2");
-        
-        if(numero1 <= numero2)
-            System.out.print("Numero 1 menor ou igual que numero 2");
-        
-        if(numero1 != numero2)
-            System.out.print("Numero 1 é diferente de numero 2");
-    }
+      int a, b;
+      a = 6;
+      b = 6;
+    
+      boolean result;
+
+      if(a==b)
+        result = true;
+      else
+        result = false;
+
+      System.out.println("Condicional: " + result);
+      };
+
 
 }
