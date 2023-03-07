8637a19b1c35df9d3e7a681d6ef866e02293955f
Martin <escorpmartin97@gmail.com>
2023-03-07 20:21:32 -0300

    feat: tipos de comentarios

--- sintaxe-java/argumentos/src/MinhaClasse.java
+++ sintaxe-java/argumentos/src/MinhaClasse.java
@@ -3,3 +3,26 @@ public class MinhaClasse {
         System.out.println("Oi, fui executado pelo terminal.");
     }
 }
+
+
+
+// Tipos de comentários:
+
+// On Line:
+    // Olá, eu sou um comentário em uma única linha
+
+// Mult Line:
+    /* Olá,
+    * Eu sou um comentario
+    * que posso ser mais detalhadod
+    * quando necessário
+    */
+
+// Documentation:
+    /** 
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
