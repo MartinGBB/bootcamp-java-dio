97012528b6d789a65d2e4dc06a74c22e7c19b30b
Martin <escorpmartin97@gmail.com>
2023-03-09 23:46:08 -0300

    feat: exercicio de nota

--- condicional-e-repeticao/loops/src/Ex2_Grade.java
+++ condicional-e-repeticao/loops/src/Ex2_Grade.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Faça um programa que peça uma nota, entre zero e dez.
 Mostre uma mensagem caso o valor seja inválido
@@ -6,5 +8,18 @@ até que o usuário informe um valor válido.
 */
 
 public class Ex2_Grade {
-  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    int grade;
+    
+    
+    System.out.println("Digite uma nota de 0 a 10:");
+    grade = scanner.nextInt();
+    
+    while(grade > 10 || grade < 0) {
+      System.out.println("Nota invalida, digite novamente");
+      grade = scanner.nextInt();
+    }
+  }
 }
