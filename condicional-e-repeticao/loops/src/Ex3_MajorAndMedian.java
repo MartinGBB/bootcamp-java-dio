1e0e8ddfd1b5cd5673cd91b6e1fcbe6465c0017c
Martin <escorpmartin97@gmail.com>
2023-03-10 17:52:27 -0300

    feat: soma e media de valores

--- condicional-e-repeticao/loops/src/Ex3_MajorAndMedian.java
+++ condicional-e-repeticao/loops/src/Ex3_MajorAndMedian.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Faça um programa que leia 5 números
 e informe o maior número
@@ -5,5 +7,28 @@ e a média desses números.
 */
 
 public class Ex3_MajorAndMedian {
-  
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+
+      int number;
+      int count = 0;
+      int major = 0;
+      int sum = 0;
+      int media = 0;
+
+      do {
+        System.out.println("Digite um numero:");
+        number = scanner.nextInt();
+
+        sum += number;
+        media = sum / 5;
+        if (major < number) major = number;
+
+        count += 1;
+      } while(count < 5);
+
+      System.out.println("O maior é: " + major);
+      System.out.println("A media é: " + media);
+    }
+  }
 }
