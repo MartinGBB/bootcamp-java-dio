8548284f64e476934609b5652bf1116709d9bf7d
Martin <escorpmartin97@gmail.com>
2023-03-10 18:27:49 -0300

    feat: seleccionando numeros impares e pares

--- condicional-e-repeticao/loops/src/Ex4_EvenAndOdd.java
+++ condicional-e-repeticao/loops/src/Ex4_EvenAndOdd.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 Faça um programa que peça N números inteiros,
 calcule e mostre a quantidade de números pares
@@ -5,5 +7,29 @@ e a quantidade de números impares.
 */
 
 public class Ex4_EvenAndOdd {
+  public static void main(String[] args) {
+    int number =  0;
+    int pair = 0;
+    int odd = 0;
+    int count = 0;
+    int quantityNumbers = 0;
+
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.println("Digite quantidade de repetição:");
+      quantityNumbers = scanner.nextInt();
   
+      do {
+        System.out.println("Digite um numero:");
+        number = scanner.nextInt();
+        count += 1;
+    
+        if (number % 2 == 0) {
+          pair += 1;
+        } else odd += 1;
+      } while(count < quantityNumbers);
+    }
+ 
+  System.out.println("Números pares: " + pair);
+  System.out.println("Números ímpares: " + odd);
+  }
 }
