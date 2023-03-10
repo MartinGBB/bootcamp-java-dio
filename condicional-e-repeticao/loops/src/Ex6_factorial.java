9d361b05cc49820808c65d76c3ee6a8f7eb04301
Martin <escorpmartin97@gmail.com>
2023-03-10 19:05:42 -0300

    feat: exercicio de fatorial

--- condicional-e-repeticao/loops/src/Ex6_factorial.java
+++ condicional-e-repeticao/loops/src/Ex6_factorial.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 /*
 Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
 Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
 */
 
 public class Ex6_factorial {
+  public static void main(String[] args) {
+    int fatorialIn;
+    int fatorialOut = 1;
   
+    try (Scanner scanner = new Scanner(System.in)) {
+      System.out.println("Insira un valor para calcular o fatorial");
+      fatorialIn = scanner.nextInt();
+
+      for (int index = fatorialIn; index >= 1; index -= 1) {
+        fatorialOut *= index;
+      }
+      System.out.println("!" + fatorialIn + " = " + fatorialOut);
+    }
+  }
 }
