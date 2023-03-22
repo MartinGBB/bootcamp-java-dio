d717dc787cd13acc8bb3d8b0f4729dd564e07d76
Martin <escorpmartin97@gmail.com>
2023-03-22 00:08:50 -0300

    feat: metodo push, referencia de no

--- estrutura-de-dados/src/conceitos/noPilhas/Pilha.java
+++ estrutura-de-dados/src/conceitos/noPilhas/Pilha.java
@@ -7,8 +7,15 @@ public class Pilha {
     this.refNoEntradaPilha = null;
   }
 
+  public void push(No novoNo) {
+    No refAuxiliar = refNoEntradaPilha;
+
+    refNoEntradaPilha = novoNo;
+    refNoEntradaPilha.setReNo(refAuxiliar);
+  }
+
   public No top() {
-    
+    return refNoEntradaPilha;
   }
 
   public boolean isEmpty() {
