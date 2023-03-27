9c9b3f8074117c022ed8544f8499feeb00b27022
Martin <escorpmartin97@gmail.com>
2023-03-27 16:44:47 -0300

    feat: adiciona isEmpty e size

---
+++ estrutura-de-dados/src/listasEncadeadas/ListaEncadeada.java
@@ -0,0 +1,35 @@
+package listasEncadeadas;
+
+import listasEncadeadas.exercicio1.No;
+
+public class ListaEncadeada<T> {
+  No<T> referenciaEntrada;
+
+  public ListaEncadeada() {
+    this.referenciaEntrada = null;
+  }
+
+  public int size() {
+    int tamanhoLista = 0;
+    No<T> referenciaAuxiliar = referenciaEntrada;
+
+    while (true) {
+      if (referenciaAuxiliar != null) {
+        tamanhoLista ++;
+        if (referenciaAuxiliar.getProximoNo() != null) {
+          referenciaAuxiliar = referenciaAuxiliar.getProximoNo();
+        } else {
+            break;
+        }
+      } else {
+          break;
+      }
+    }
+
+    return tamanhoLista;
+  }
+
+  public boolean isEmpty() {
+    return referenciaEntrada != null;
+  }
+}
